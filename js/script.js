11a4eca681179e3cff0049397855661c9d94135f
Vahidone <v.ban.roma@gmail.com>
2023-09-21 15:45:24 +0200

    feat: create point 1 of readme

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+// punto 1 
+const team = [
+
+  {
+    nome: 'Manuel Neuer', 
+    ruolo: 'portiere',
+    foto: "img/Manuel Neuer.png",
+  },
+  {
+    nome: 'Paolo Maldini',
+    ruolo: 'difensore',
+    foto: "img/Paolo Maldini.jpg",
+  },
+  {
+    nome: 'Zinédine Zidane',
+    ruolo: 'centrocampista',
+    foto: "img/Zinédine Zidane.jpeg",
+  },
+  {
+    nome: 'Diego Maradona',
+    ruolo: 'centravanti',
+    foto: "img/Diego Maradona.jpg",
+  },
+  {
+    nome: 'Francesco Totti',
+    ruolo: 'attaccante',
+    foto: "img/Francesco Totti.jpg",
+  }, 
+
+];
+
