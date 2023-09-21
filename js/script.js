18620b5b903c69b7c6ca8a5d1d6e55fa7d27dc5b
Vahidone <v.ban.roma@gmail.com>
2023-09-21 21:32:20 +0200

    style: change styling of title

--- js/script.js
+++ js/script.js
@@ -42,8 +42,8 @@ for (const player of team) {
   const teamMembers = document.querySelector('.team-members');
   
   teamMembers.innerHTML += `
-  <div class="card m-2  text-center" style="width: 270px;">
-    <img src="${player.foto}" style="width: 100%; height: 100%; object-fit: cover">
+  <div class="card m-2 text-center" style="width: 270px; cursor: pointer; ">
+    <img src="${player.foto}" style="width: 100%; height: 100%; object-fit: cover; border-radius: 10px;">
     <div class="card-body">
       <h5 class="card-title text-primary fw-bold">${player.nome}</h5>
       <p class="card-text fw-bold">${player.ruolo}</p>
